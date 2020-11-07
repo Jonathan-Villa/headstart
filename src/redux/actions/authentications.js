c625b46bdbff10ddd6a644622ba4bbb802fe7a8d
Jonathan-Villa <jonathanvilla03@gmail.com>
2020-11-06 22:40:41 -0600

    updated -jv

--- src/redux/actions/authentications.js
+++ src/redux/actions/authentications.js
@@ -1,10 +1,23 @@
 import axios from 'axios'
 import {GET_ERRORS} from './actionTypes'
 
-export const registerNewUser = (user, history) => (dispatch) => {
+export const registerNewUser = (user, history) => dispatch => {
     axios.post("http://localhost:4000/api/signup", user)
     .then(res => history.push('/login'))
     .catch(err => {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    })
+}
+
+export const loginUser = (user) => dispatch => {
+    axios.post("http://localhost:4000/api/login", user)
+    .then(res =>{
+        console.log(res)
+    })
+    .catch(err =>{
         dispatch({
             type: GET_ERRORS,
             payload: err.response.data
