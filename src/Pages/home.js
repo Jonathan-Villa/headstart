de23bce0af1d698d383fbc169efc877559960a80
oliver <swoliverro@lewisu.edu>
2020-11-20 15:40:21 -0600

    Updated Home

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,14 +1,47 @@
 import React from "react";
 import "./styles/home.css";
-import { useSelector} from "react-redux"
-import HeroSection from '../components/HeroSection';
+import {makeStyles} from "@material-ui/core/styles"
 
+import {Grid,} from '@material-ui/core';
+import DataTable from "../components/DataTable/datatable";
+import Header from "../components/Header/Header"
+import HeroSection from "../components/HeroSection";
+
+const useStyles = makeStyles((theme) => ({
+  grid: {
+      width : '100%' ,
+      margin:'0px',
+  },
+
+
+  paper:{
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      
+  }
+
+}));
 function Home() {
 
+  const classes = useStyles()
   return (
-    <>
-    <HeroSection />
-    </>
+    <Grid container spacing ={4} className = {classes.grid}>
+       <Grid item xs={12} md = {12}>
+        <Header/>
+    </Grid>
+      <Grid item xs={12} md = {6}>
+        <DataTable/>
+    </Grid>
+    <Grid item xs={12} md = {6}>
+        <DataTable/>
+    </Grid>
+    <Grid item xs={12} md = {6}>
+        <HeroSection/>
+    </Grid>
+
+</Grid>
+
   );
 }
 
