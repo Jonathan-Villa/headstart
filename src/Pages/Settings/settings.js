c53990be4fecfa62c3744551b441ee217d73ded0
bob <robertcoliver@lewisu.edu>
2020-12-12 17:36:32 -1000

    added the Setting component - roj

--- src/Pages/Settings/settings.js
+++ src/Pages/Settings/settings.js
@@ -5,10 +5,20 @@ import { StudentSettings } from "../../components/student";
 import { AdminSettings } from "../../components/admin";
 import {Paper} from "@material-ui/core"
 import { useStyles} from "./styles"
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
 function Settings() {
   const isAdminRole = useSelector((state) => state.loginReducer.role);
   const [user, setUser]= useState()
   const styles = useStyles()
+  const classes = useStyles();
+
 
   useEffect(()=> {
     const getUser =()=> {
@@ -17,8 +27,38 @@ function Settings() {
     getUser()
   },[isAdminRole])
 
+  function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+  }
+
   return (
     <div className="main-container">
+      
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+         
+          <ListItemText primary="First Name:" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+         
+          <ListItemText primary="Last Name:" />
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <ListItemText primary = "Account Type:"/>
+
+        </ListItem>
+        <Divider />
+        <ListItem button>
+         
+          <ListItemText primary="CLICK HERE TO PERMANENTLY DELETE YOUR ACCOUNT" />
+        </ListItem>
+      </List>
+      
+      <Divider />
+      
+    
       <Paper className={styles.paper} elevation={3}>
         {user === "admin" ? <AdminSettings /> : <StudentSettings />}
       </Paper>
--- src/Pages/Settings/styles.js
+++ src/Pages/Settings/styles.js
@@ -1,10 +1,19 @@
 import {makeStyles} from "@material-ui/core"
+import { CenteredGrid } from "../../components/CenteredGrid/centeredGrid"
 
 
-export const useStyles = makeStyles(()=>({
+export const useStyles = makeStyles((theme)=>({
     paper:{
         width:"85%",
         height:"90%", 
      
-    }
+    },
+    root: {
+        padding: '0 30px',
+        textAlign: true,
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+        
+      }
 }))
